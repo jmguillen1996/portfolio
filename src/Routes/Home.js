f039198256ee7fdeb96a8ed04b34e1047ec381a6
jmguillen1996 <josefmguillen@gmail.com>
2024-10-17 22:57:57 +0800

    Created basic footer.

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <h1>Home</h1>
+            <Footer />
         </div>
     );
 }
--- src/Routes/Project.js
+++ src/Routes/Project.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const Project = () => {
     return (
         <div>
             <Header />
             <h1>Project</h1>
+            <Footer />
         </div>
     );
 }
