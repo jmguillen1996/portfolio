846c0924c9d3c80634283fe280d37681e456e224
jmguillen1996 <josefmguillen@gmail.com>
2024-10-18 02:50:28 +0800

    Initial header design.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import './Styles/Header.css';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <div>
-            <p>
-                <Link to="/">Home</Link>
-            </p>
-            <p>
-                <Link to="/project">Project</Link>
-            </p>
-        </div>
+        <nav>
+            <Link to="/" className="title">josefguillen</Link>
+            <div className="menu" onClick={() => {
+                setMenuOpen(!menuOpen);
+            }}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ""}>
+                <li><NavLink to='/home'>Home</NavLink></li>
+                <li><NavLink to="/">Skill</NavLink></li>
+                <li><NavLink to="/">Project</NavLink></li>
+                <li><NavLink to="/">Work Experience</NavLink></li>
+                <li><NavLink to="/">Contact</NavLink></li>
+            </ul>
+        </nav>
     );
 }
 
