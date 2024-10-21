12fdb365b8db4744e2fe2696f085ec6f47760578
jmguillen1996 <josefmguillen@gmail.com>
2024-10-21 23:21:20 +0800

    Nav bar dynamic implementation.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,61 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import './Styles/Header.css';
+import { FiDownload, FiMenu, FiX } from 'react-icons/fi';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     return (
-        <nav>
-            <Link to="/" className="title">josefguillen</Link>
-            <div className="menu" onClick={() => {
-                setMenuOpen(!menuOpen);
-            }}>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+        <div className="parent-container">
+            <div class="nav-container">
+                <div className="title-container">
+                    <Link to="/" className="title">
+                        <h3>
+                            <span>josef</span>
+                            guillen
+                        </h3>
+                    </Link>
+                    <div className="menu-action-container" onClick={() => {
+                        setMenuOpen(!menuOpen);
+                    }}>
+                        {menuOpen ? <FiX size={25} /> : <FiMenu size={25} />}
+                    </div>
+                </div>
+                <div className={menuOpen ? "item-container open" : "item-container close"}>
+                    <div className="menu">
+                        <div className="menu-item">Home</div>
+                        <div className="menu-item">Skill</div>
+                        <div className="menu-item">Project</div>
+                        <div className="menu-item">Work Experience</div>
+                        <div className="menu-item">Contact</div>
+                    </div>
+                    <button className="btn-download-resume">
+                        <FiDownload size={18} color="white" />
+                        Resume
+                    </button>
+                </div>
             </div>
-            <ul className={menuOpen ? "open" : ""}>
-                <li><NavLink to='/home'>Home</NavLink></li>
-                <li><NavLink to="/">Skill</NavLink></li>
-                <li><NavLink to="/">Project</NavLink></li>
-                <li><NavLink to="/">Work Experience</NavLink></li>
-                <li><NavLink to="/">Contact</NavLink></li>
-            </ul>
-        </nav>
+            {/* <nav>
+
+                <div className="menu" onClick={() => {
+                    setMenuOpen(!menuOpen);
+                }}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className={menuOpen ? "open" : ""}>
+                    <li><NavLink to='/home'>Home</NavLink></li>
+                    <li><NavLink to="/">Skill</NavLink></li>
+                    <li><NavLink to="/">Project</NavLink></li>
+                    <li><NavLink to="/">Work Experience</NavLink></li>
+                    <li><NavLink to="/">Contact</NavLink></li>
+                </ul>
+            </nav> */}
+        </div>
     );
 }
 
