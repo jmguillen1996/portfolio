55be4f1fd067113eb8e27987e8ecf4d928d203de
jmguillen1996 <josefmguillen@gmail.com>
2024-10-25 14:31:56 +0800

    Created function to populate work experience.

--- src/Components/HomeExperience.js
+++ src/Components/HomeExperience.js
@@ -3,10 +3,29 @@ import "./Styles/HomeExperience.css";
 import work_history from "../Data/work_history.json";
 
 const HomeExperience = () => {
-    return(
+    return (
         <div className="experience-container">
             <h2>Work History</h2>
             <h1>Professional Experience</h1>
+            <div className="work-history-list-container">
+                {
+                    work_history.map(
+                        (work, index) => (
+                            <div className="work-item" key={index}>
+                                <h1 className="work-position">
+                                    {work.position}
+                                </h1>
+                                <div className="work-address">
+                                    {work.company}, {work.address} {work.others}
+                                </div>
+                                <div className="work-timeline">
+                                    {work.timeline}
+                                </div>
+                            </div>
+                        )
+                    )
+                }
+            </div>
         </div>
     );
 }
