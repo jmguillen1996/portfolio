8f3edb9bbe9a9ff31691592996f8f6d48ea49455
jmguillen1996 <josefmguillen@gmail.com>
2024-11-18 01:30:15 +0800

    Other adjustment to make the ui responsive.

--- src/Components/Home/HomeGetStarted.js
+++ src/Components/Home/HomeGetStarted.js
@@ -4,7 +4,21 @@ import '../Styles/Home/HomeGetStarted.css';
 const HomeGetStarted = () => {
     return (
         <div className="get-started-container">
-           
+            <div className="back-container">
+                <div className="upper-back"></div>
+                <div className="lower-back"></div>
+            </div>
+            <div className="get-started-item-container">
+                <div className="get-started-content">
+                    <div className="content-message">
+                        <h1>Ready to <span>get started?</span></h1>
+                        <h1>Contact me today!</h1>
+                    </div>
+                    <button className="btn-send-me-email">
+                        Send me an email
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
